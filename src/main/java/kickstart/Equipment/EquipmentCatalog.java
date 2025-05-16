b4f79e6cb0b09f80657a51c32b81fc2a8b98e114
DanielDeyD <164528803+DanielDeyD@users.noreply.github.com>
2025-05-16 19:29:35 +0200

    Delete src directory and update to new src

--- src/main/java/kickstart/Equipment/EquipmentCatalog.java
+++
@@ -1,9 +0,0 @@
-package kickstart.Equipment;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-import java.util.UUID;
-
-public interface EquipmentCatalog extends CrudRepository<Equipment, UUID> {
-    List<Equipment> findByType(String type);
-}
