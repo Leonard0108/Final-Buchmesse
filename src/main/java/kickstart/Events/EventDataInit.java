76a1b877eff986445ebac9cc20364e87ac4705b4
Ryokazus <alexanderschuster2020@gmail.com>
2025-05-17 17:21:17 +0200

    Changes the initilaized Testdata

--- src/main/java/kickstart/Events/EventDataInit.java
+++ src/main/java/kickstart/Events/EventDataInit.java
@@ -1,6 +1,5 @@
 package kickstart.Events;
 
-import org.javamoney.moneta.Money;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,39 +16,39 @@ public class EventDataInit implements CommandLineRunner {
     public void run(String... args) throws Exception {
         // Creating and saving events with the existing constructor
         eventCatalog.save(new Event(
-            "Tech Conference 2025",
-            "A conference bringing together tech enthusiasts and industry leaders.",
-            Money.of(99.99, "USD"),
+            "Gardening Fair 2025",
+            "Discover inspiring reads, green ideas, and expert knowledge from the world of horticulture",
+           60.90,
             "2025-06-15",
-            "Berlin, Germany",
+            "Leifurter Exhibition Grounds, Hall A & B",
             "Upcoming",
             "Visitors: 1000",
-            "Conference",
-            "http://example.com/tech-conference-image.jpg"
+            "Gardening",
+            "Gardening_Event_2025.png"
         ));
 
         eventCatalog.save(new Event(
-            "Music Festival 2025",
-            "An exciting music festival with top bands and performers.",
-            Money.of(49.99, "USD"),
+            "Manga Fair 2025",
+            "Dive into captivating worlds, unforgettable characters, and thrilling stories from the realm of Japanese comics!",
+            50.00,
             "2025-07-20",
-            "Munich, Germany",
+			"Leifurter Exhibition Grounds, Hall C",
             "Upcoming",
             "Visitors: 5000",
-            "Festival",
-            "http://example.com/music-festival-image.jpg"
+            "Manga Comic con",
+            "Manga_2025.png"
         ));
 
         eventCatalog.save(new Event(
-            "Startup Bootcamp",
-            "A bootcamp for aspiring entrepreneurs to learn and grow their businesses.",
-            Money.of(299.99, "USD"),
+            "Politics Fair 2025",
+            "Explore insightful analyses, lively debates, and visionary ideas shaping our society and future!",
+            30.10,
             "2025-08-05",
-            "Hamburg, Germany",
+			"Leifurter Exhibition Grounds, Hall B & C",
             "Upcoming",
             "Visitors: 200",
-            "Workshop",
-            "http://example.com/startup-bootcamp-image.jpg"
+            "Politics",
+            "Politics_2025.png"
         ));
 
         System.out.println("Sample events added to the database.");
