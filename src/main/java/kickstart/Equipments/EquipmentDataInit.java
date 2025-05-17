52570510a1400411ad1adc8f87c81a147eec826c
Leon <leonard.setiadji@gmail.com>
2025-05-17 16:53:22 +0200

    updated datenbank

--- src/main/java/kickstart/Equipments/EquipmentDataInit.java
+++ src/main/java/kickstart/Equipments/EquipmentDataInit.java
@@ -4,37 +4,30 @@ import org.javamoney.moneta.Money;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.StreamSupport;
+
 @Component
 public class EquipmentDataInit implements CommandLineRunner {
 
-    private final EquipmentCatalog equipmentCatalog;
+	private final EquipmentCatalog equipmentCatalog;
 
-    public EquipmentDataInit(EquipmentCatalog equipmentCatalog) {
-        this.equipmentCatalog = equipmentCatalog;
-    }
+	public EquipmentDataInit(EquipmentCatalog equipmentCatalog) {
+		this.equipmentCatalog = equipmentCatalog;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Creating and saving events with the existing constructor
-        equipmentCatalog.save(new Equipment(
-            "Camera",
-            Money.of(99.99, "USD"),
-            "Lighting",
-            "http://example.com/tech-conference-image.jpg"
-        ));
-        equipmentCatalog.save(new Equipment(
-            "Chair",
-            Money.of(45, "USD"),
-            "Misc",
-            "http://example.com/tech-conference-image.jpg"
-        ));
-        equipmentCatalog.save(new Equipment(
-            "Speaker",
-            Money.of(199.99, "USD"),
-            "Sound",
-            "http://example.com/tech-conference-image.jpg"
-        ));
+	@Override
+	public void run(String... args) throws Exception {
+		createEquipmentIfNotExists("Camera", Money.of(99.99, "USD"), "Lighting", "http://example.com/tech-conference-image.jpg");
+		createEquipmentIfNotExists("Chair", Money.of(45, "USD"), "Misc", "http://example.com/tech-conference-image.jpg");
+		createEquipmentIfNotExists("Speaker", Money.of(199.99, "USD"), "Sound", "http://example.com/tech-conference-image.jpg");
+	}
 
-        System.out.println("Sample eq added to the database.");
-    }
+	private void createEquipmentIfNotExists(String name, Money price, String type, String imageUrl) {
+		boolean exists = StreamSupport.stream(equipmentCatalog.findAll().spliterator(), false)
+			.anyMatch(e -> e.getName().equalsIgnoreCase(name));
+
+		if (!exists) {
+			equipmentCatalog.save(new Equipment(name, price, type, imageUrl));
+		}
+	}
 }
